c99d3195906cf50f322a13b1e15ba3379949607a
pc1995 <1247956700@qq.com>
2017-09-01 14:51:05 +0800

    vuex文件

--- src/store/actions.js
+++ src/store/actions.js
@@ -56,6 +56,7 @@ export const insertSong = function ({commit, state}, song) {
     }
   }
 
+
   let currentSIndex = findIndex(sequencelist, currentSong) + 1
 
   let fsIndex = findIndex(sequencelist, song)
